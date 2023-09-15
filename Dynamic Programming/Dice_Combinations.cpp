fddc50fd63f0726cc14c5b58ea7502b76d6ecf03
Minhaj <minhajulislammuhammad@gmail.com>
2023-09-15 16:36:13 +0600

    added DP

---
+++ Dynamic Programming/Dice_Combinations.cpp
@@ -0,0 +1,23 @@
+// Q: https://cses.fi/problemset/task/1633
+#include<bits/stdc++.h>
+using namespace std;
+const int mod = 1e9+7;
+int dp[1000002];
+int fn(int n)
+{
+   if(n == 0) return 1;
+   if(n < 0) return 0;
+   if(dp[n] != -1) return dp[n];
+   int ans = 0;
+   for(int i = 1; i <= 6; i++)
+    ans = (ans + fn(n-i))%mod;
+    return dp[n] = ans;
+}
+int main()
+{
+    memset(dp, -1, sizeof(dp));
+    int n;
+    cin >> n;
+    cout << fn(n) << "\n";
+    return 0;
+}
